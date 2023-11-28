25b0735a3b4d5068e1a6aeefefe31abe98e498c2
Matheus Davi Félix Migliani França <128201513+MatheusMigliani@users.noreply.github.com>
2023-11-28 20:17:44 -0300

    Update 2014_10_12_200000_add_two_factor_columns_to_users_table.php

--- database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
+++ database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
@@ -1,9 +1,6 @@
-<?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
 
 return new class extends Migration
 {
@@ -12,8 +9,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-        
+        Schema::create('users', function (Blueprint $table) {
+            
+
         });
     }
 
@@ -22,13 +20,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn(array_merge([
-                'two_factor_secret',
-                'two_factor_recovery_codes',
-            ], Fortify::confirmsTwoFactorAuthentication() ? [
-                'two_factor_confirmed_at',
-            ] : []));
-        });
+        Schema::dropIfExists('users');
     }
 };
